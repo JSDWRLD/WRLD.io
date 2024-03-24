b92625547fca145c726856be3ef6f0ab4caaced2
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-03-24 12:55:30 -0700

    Created default components ready for work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
+// created index.js in pages to simplify import 1 line > 4 lines of text
+import { Home, About, Projects, Contact } from './pages';
+
 const App = () => {
   return (
     <main className='bg-slate-300/20'>
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element={'Home'} />
-                <Route path="/about" element={'About'} />
-                <Route path="/projects" element={'Projects'} />
-                <Route path="/contact" element={'Contact'} />
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </Router>
     </main>
