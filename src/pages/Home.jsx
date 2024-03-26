07873bbb9674dd8580f93296a668642a60681421
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-03-26 12:27:39 -0700

    First .glb 3d component implemented and rendered on react-three Canvas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Loader  from '../components/Loader';
+import Island from '../models/Island';
+
+{/*<div className="className absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div>*/}
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='w-full h-screen relative'>
+      <Canvas 
+        className='w-full h-screen bg-transparent'
+        camera={{ position:[100, 5, 100], near:0.1, far: 1000 }}
+      >
+
+      <Suspense fallback={<Loader />}>
+        <directionalLight />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
+
+        <Island />
+      </Suspense>
+
+      </Canvas>
+    </section>
   )
 }
 
