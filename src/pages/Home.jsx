ff5c9434429901c1fcc499bec040ef41d99ebdbf
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-03-31 20:44:28 -0700

    Island fully rendered and positioned, eslint disabled for now.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,21 +8,39 @@ import Island from '../models/Island';
       </div>*/}
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if(window.innerWidth < 768){
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [ screenScale, screenPosition, rotation ]
+  }
+
+  const [ islandScale, islandPosition, islandRotation ] = adjustIslandForScreenSize();
+
   return (
     <section className='w-full h-screen relative'>
       <Canvas 
         className='w-full h-screen bg-transparent'
-        camera={{ position:[100, 5, 100], near:0.1, far: 1000 }}
+        camera={{ near:0.1, far: 1000 }}
       >
 
       <Suspense fallback={<Loader />}>
-        <directionalLight />
-        <ambientLight />
-        <pointLight />
-        <spotLight />
-        <hemisphereLight />
+        <directionalLight position={[1, 1, 1]} intensity={2} />
+        <ambientLight intensity={0.5} />
+        <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
-        <Island />
+        <Island 
+          position = {islandPosition}
+          scale = {islandScale}
+          rotation = {islandRotation}
+        />
       </Suspense>
 
       </Canvas>
