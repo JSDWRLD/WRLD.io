16894dba38c53f69aec9a8b1c784a41a660bb48a
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-04-06 21:49:37 -0700

    Sky integrated 3d sphere

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Loader  from '../components/Loader';
 import Island from '../models/Island';
+import Sky from '../models/Sky';
 
 {/*<div className="className absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         POPUP
@@ -36,6 +37,7 @@ const Home = () => {
         <ambientLight intensity={0.5} />
         <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
+        <Sky />
         <Island 
           position = {islandPosition}
           scale = {islandScale}
