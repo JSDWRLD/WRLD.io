b16e86b9b84678c0f4814efea08143a20fb2daf9
DJ <123333887+JSDWRLD@users.noreply.github.com>
2024-04-09 16:31:43 -0700

    bug fix, laggy scroll

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -78,6 +78,8 @@ const Island = ({isRotating, setIsRotating, ...props }) => {
       rotationSpeed.current *= dampingFactor;
 
       if(Math.abs(rotationSpeed.current < 0.001)) rotationSpeed.current = 0;
+
+      islandRef.current.rotation.y += rotationSpeed.current;
     } else {
       const rotation = islandRef.current.rotation.y;
 
